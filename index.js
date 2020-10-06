7a6c5cff32425180bc89e1ba2bf114b79bea6016
sid@root <sidathwip@aol.com>
2020-10-06 22:28:32 +0200

    can't scrape src from video tag

--- index.js
+++ index.js
@@ -4,21 +4,35 @@ const fs		= require( "fs" );
 const express 	= require( "express" );
 const bodyParse = require( "body-parser" );
 
+// port
 const port = 3000;
 
 const app = express();
+
+// middlewares
 app.use( express.static( 'public' ));
 app.use( bodyParse.json() );
 app.use( bodyParse.urlencoded({
 	extended: true
 }) )
 
+// routes
 app.get( "/", ( req, res ) => {
 	res.sendFile("index.html");
 });
 
 app.post( "/", ( req, res, next ) => {
 	let url = req.body.redditUrl;
+	console.log( `requested video download url: ${url}` );
+
+	request( url, ( err, reqRes, html ) => {
+		if( !err && reqRes.statusCode == 200 ) {
+			const $ = cheerio.load( html, { decodeEntities: false, withDomLvl1: false });
+
+			console.log( $( "video" ).attr( "src" ));
+		}
+	});
+	res.send("ok")
 });
 
 
