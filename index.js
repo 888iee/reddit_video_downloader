08c16836d932316c6e38369d1c0964fa6060ce14
sid@root <sidathwip@aol.com>
2020-10-13 17:57:25 +0200

    enabled client side html

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
-// const reddit = require( "./reddit" );
-const fs = require( "fs" );
-const download = require( "./download" );
+const fs 		= require( "fs" );
+const download 	= require( "./download" );
+const express 	= require( "express" );
+const bodyParse = require( "body-parser" );
 
 // port
 const port = 3000;
 // debugger;
 
+// DEBUGGING WITHOUT CLIENTSIDE HTML
+// let url = "https://reddit.com/r/WatchPeopleDieInside/comments/j8n5c1/putin_laughs_at_his_minister_for_suggesting_to/";
+// // reddit.initialize( url );
 
-let url = "https://reddit.com/r/WatchPeopleDieInside/comments/j8n5c1/putin_laughs_at_his_minister_for_suggesting_to/";
-// reddit.initialize( url );
+// download.initialize( url );
 
-download.initialize( url );
+const app = express();
+
+// middlewares
+app.use( express.static( 'public' ));
+app.use( bodyParse.json() );
+app.use( bodyParse.urlencoded({
+	extended: true
+}));
+
+// routes
+app.get( "/", ( req, res ) => {
+	res.sendFile("index.html");
+});
+
+app.post( "/", ( req, res, next ) => {
+	let url = req.body.redditUrl;
+	console.log( `requested video download url: ${url}` );
+
+	
+	download.initialize( url );
+});
+
+app.listen( port, () => console.log( `Server running on port ${ port }` ));
