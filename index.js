17c29a8f71c83981fa3d5cfd8e94b364a60b2a68
sid@root <sidathwip@aol.com>
2020-11-24 11:41:56 +0100

    refactoring

--- index.js
+++ index.js
@@ -5,20 +5,6 @@ const path 		= require( "path" );
 
 // port
 const port = 3000;
-// debugger;
-
-// DEBUGGING WITHOUT CLIENTSIDE HTML
-// reddit.initialize( url );
-// let url = "https://reddit.com/r/WatchPeopleDieInside/comments/j8n5c1/putin_laughs_at_his_minister_for_suggesting_to/";
-
-// const f = new Download( url );
-
-// f.startProcessing();
-
-// const g = new Download( "https://www.reddit.com/r/Damnthatsinteresting/comments/jhpegj/how_companies_label_sugar_to_trick_you_into/" );
-// g.startProcessing();
-
-
 
 const app = express();
 
@@ -50,8 +36,3 @@ app.post( "/", ( req, res, next ) => {
 });
 
 app.listen( port, () => console.log( `Server running on port ${ port }` ));
-
-// https://stackoverflow.com/questions/56615083/executing-a-function-after-an-another-asynchronous-function-finishes-in-javasc
-// https://superuser.com/questions/277642/how-to-merge-audio-and-video-file-in-ffmpeg
-// https://github.com/damianociarla/node-ffmpeg
-// https://stackoverflow.com/questions/28790744/how-would-one-do-async-javascript-getters-and-setters
